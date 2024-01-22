a48303c019b421d5d9c694c4939532d02b3b5bba
dealmeida-dev <102060897+dealmeida-dev@users.noreply.github.com>
2024-01-22 15:11:21 +0100

    initial commit

---
+++ 01-Fundamentals-Part-1/starter/exercises.js
@@ -0,0 +1,43 @@
+// // Part 1
+
+// // Exercise 1
+
+// let country = "Switzerland";
+// const continent = "Europe";
+// let population = 87000000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// // Exercise 2
+
+// const isIsland = false;
+
+// let language;
+
+// console.log(isIsland);
+
+// // Exercise 3
+
+// language = "German";
+
+// console.log(language);
+
+// // Exercise 4
+
+// console.log((population /= 2), (population /= 2));
+
+// population += 1;
+// console.log(population);
+
+// const finland = 6000000;
+
+// console.log(country > finland);
+
+// const averagePopulation = 33000000;
+
+// console.log(population > averagePopulation);
+
+// const description =
+//   "Portugal is in Europe, and its 11 million people speak portuguese";
---
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+
+let delfim = "buff";
+
+let year = 2024;
+
+const ageDelfim = year - 1991;
+const ageJack = year - 2013;
+
+console.log(ageDelfim * 2, ageJack / 3, year ** 3);
+
+console.log(js + " " + delfim);
+
+let x = 10 + 5;
+
+x += 10;
+
+console.log(x);
+
+console.log(x >= year);
